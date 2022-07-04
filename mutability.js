6056fa9f9273a71c6799510c12f7c01a0326753f
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-04 12:53:14 +0100

    declarations

--- mutability.js
+++ mutability.js
@@ -1,8 +1,8 @@
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-};
+// const person = {
+//     name: 'Rick',
+//     age: 77,
+//     country: 'US',
+// };
 
 const clone1 = {
     person: {
