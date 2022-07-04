f7eb9ec6950b3f52c15de9a4b2c0779eada0f19c
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-04 13:32:45 +0100

    declarations

--- mutability.js
+++ mutability.js
@@ -1,16 +1,21 @@
+// const person = {
+//     name: 'Rick',
+//     age: 77,
+//     country: 'US',
+// }  
+
 let clone1 = { ...person }
 let clone2 = { ...person }
 
-clone1.person.age = 78
-clone1.person.country = 'FR'
+clone1.age = 78
+clone1.country = 'FR'
+Object.freeze(clone1)
 
 clone2.age = 78
 clone2.country = 'FR'
+Object.freeze(clone2)
 
 let samePerson = person
-
-Object.freeze(clone1)
-Object.freeze(clone2)
 
 console.log(clone1)
 console.log(clone2)
