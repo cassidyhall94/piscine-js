1339435fcb03dc307bd73d04c123dee7a7f90ce3
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-04 19:22:08 +0100

    dogyears

--- dog-years.js
+++ dog-years.js
@@ -1,5 +1,17 @@
+
+// earth 1.0 Earth years, 365.25 Earth days, or 31,557,600 seconds.
+earth = 1
+mercury = 0.2408467
+venus = 0.61519726
+mars = 1.8808158 
+jupiter = 11.862615 
+saturn = 29.447498
+uranus = 84.016846
+neptune = 164.79132 
+
 function dogYears(planet, dogAge) {
-return Math.round(((dogAge/planet) * 7) * 100) / 100
+    let dogYears = (dogAge/31557600) * 7
+return Math.round((dogYears * 100) / 100)
 }
 
-// console.log(dogYears(31557600, 1000000000))
+console.log(dogYears(earth, 1000000000))
