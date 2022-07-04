6987a290bccbdb6ce558c780c3274790662d9fa6
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-04 16:53:34 +0100

    last,first,kiss

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,3 +1,7 @@
 const first = (n) => n[0]
 const last = (n) => n[n.length-1]
-const kiss = (n) => last(n) + first(n)
+const kiss = (n) => [last(n) + first(n)]
+
+console.log(first("hey"))
+console.log(last("hey"))
+console.log(kiss("hey"))
