c530b7c7284cfa314ac141789bfba4457328536f
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-04 11:56:54 +0100

    declarations

--- declarations.js
+++ declarations.js
@@ -1,23 +1,21 @@
 const escapeStr = String.raw`\\\`"'/`;
-const arr = [4, '2'];
-Object.freeze(arr);
-const obj = {
+const arr = Object.freeze([4, '2']);
+const obj = Object.freeze({
     str: "Hello World",
     num: 0,
     bool:  false,
     undef: undefined,
-};
-Object.freeze(obj);
-const nested = {
+});
+const nested = Object.freeze({
     arr: [4, obj.undef, '2'],
     obj: {
         str: "Hello World",
         num: 0,
         bool:  false,
     },
-};
-Object.freeze(nested);
+});
 
+console.log(escapeStr)
 console.log(arr)
 console.log(obj)
 console.log(nested)
