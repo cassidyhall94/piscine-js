9e4a915c66cd11cffcb1744dabc453735177f706
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-04 12:44:56 +0100

    declarations

--- mutability.js
+++ mutability.js
@@ -1,12 +1,9 @@
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-};
-
-// const clone1 = person ={
-//     Object.assign(78, person.age), Object.assign('FR', person.country)
+// const person = {
+//     name: 'Rick',
+//     age: 77,
+//     country: 'US',
 // };
+
 const clone1 = {
     person: {
         age: 78,
