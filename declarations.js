9574b2b0f18245c3859de6ac208e35bfd2886caf
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-04 12:08:23 +0100

    declarations

--- declarations.js
+++ declarations.js
@@ -7,16 +7,15 @@ const obj = {
     undef: undefined,
 };
 const nested = {
-    arr: [4, obj.undef, '2'],
-    obj: {
+    arr: Object.freeze([4, obj.undef, '2']),
+    obj: Object.freeze({
         str: "Hello World",
         num: 0,
         bool:  false,
-    },
+    }),
 };
 
-Object.freeze(nested.arr)
-Object.freeze(nested.obj)
+
 console.log(escapeStr)
 console.log(arr)
 console.log(obj)
