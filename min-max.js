0102a8399c8b0c9c8d44c13a86ef9f14451918a8
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-05 16:25:52 +0100

    min-max

---
+++ min-max.js
@@ -0,0 +1,17 @@
+function max(n, m) {
+    if (n > m) {
+        return n
+    }
+    if (m > n) {
+        return m
+    }
+}
+
+function min(n, m) {
+    if (n < m) {
+        return n
+    }
+    if (m < n) {
+        return m
+    }
+}
