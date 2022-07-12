bed0c2a72120983327f9296a17d6617e54bc3c0c
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-12 19:29:16 +0100

    tuesday

--- count-leap-years.js
+++ count-leap-years.js
@@ -7,13 +7,9 @@ function countLeapYears(date = new Date()) {
         return 0
     }
     for (let i = epochYear; i < year; i++) {
-        if (leapYear(i)) {
+        if (((i % 4 == 0) && (i % 100 != 0)) || (i % 400 == 0)) {
             count++
         }
     }
     return count - 1
-}
-403
-function leapYear(year) {
-    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
