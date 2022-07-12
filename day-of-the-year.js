b8ade0d68b2dc35dc858b4406a0245450570806e
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-12 20:09:30 +0100

    tuesday

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,3 +1,13 @@
-function dayOfTheYear(date) {
-    
+function dayOfTheYear(date = new Date()) {
+    let year = date.getFullYear();
+    // console.log(year)
+    if (year === 0) {
+        return 1
+    } else {
+        let startDate = new Date("01-01-" + year)
+        let difference = date.getTime() - startDate.getTime();
+        let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+        console.log(TotalDays)
+        return TotalDays+1;
+    }
 }
