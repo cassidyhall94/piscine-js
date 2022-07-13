d181a00b4e34c4430841fbfe2ff08a66e392f0dc
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-13 18:17:32 +0100

    wednesday

--- using-map.js
+++ using-map.js
@@ -1,6 +1,35 @@
-function citiesOnly(cities) {
-    let result = [cities.city]
-    return result;
+const citiesOnly = (objArr) => objArr.map(x => x.city);
+const upperCasingStates = (objArr) => objArr.map(x => capitalize(x));
+const fahrenheitToCelsius = (arr) => arr.map(x => split(x));
+
+const trimTemp = (objArr) => objArr.map(x => {
+    if (x.temperature = x.temperature.replace(/ /g, "")) {
+        return x
+    }
+});
+
+const tempForecasts = (objArr) => objArr.map(x => {
+    return split(x.temperature) + "elsius in " + x.city + ', ' + capitalizeFirstLetter(x.state)
+});
+
+function capitalize(s) {
+    const words = s.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    return words.join(" ")
 }
 
-console.log(cities.map(citiesOnly))
+function capitalizeFirstLetter(s) {
+    const words = s.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ")
+}
+
+function split(s) {
+    const nums = s.split('°F');
+    let result = (nums[0] - 32) * 5 / 9
+    return Math.floor(result) + '°C'
+}
