83c2e80e41a0d32ab3f4fc6cb65d09ffcb803977
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-13 13:38:26 +0100

    wednesday

--- circular.js
+++ circular.js
@@ -1,2 +1,3 @@
 let circular = {};
-circular.circular = circular;
+// circular.circular = circular;
+// console.log('hello')
---
+++ using-map.js
@@ -0,0 +1,6 @@
+function citiesOnly(cities) {
+    let result = [cities.city]
+    return result;
+}
+
+console.log(cities.map(citiesOnly))
