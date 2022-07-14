7731fff147dff012a6fbb1c10575550c15ff32de
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-14 12:43:34 +0100

    thursday

---
+++ using-reduce.js
@@ -0,0 +1,17 @@
+
+const adder = (arr, initialValue = 0) => arr.reduce((x, y) =>
+    x + y, initialValue
+);
+
+const sumOrMul = (arr, initialValue = 0) => arr.reduce(function (x, y) {
+    if (y % 2 === 0) {
+        return x * y
+    } else {
+        return x + y
+    }
+}, initialValue);
+
+const funcExec = (arr, initialValue) => arr.reduce(
+    (x, y) => y(x),
+    initialValue
+)
