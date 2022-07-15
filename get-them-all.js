4badceef003a6e26fa66a981515c4e5c6b134af0
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-15 17:34:21 +0100

    friday

--- get-them-all.js
+++ get-them-all.js
@@ -1,26 +1,25 @@
-export const getArchitects = () => {
-    const y = document.querySelectorAll("a");
-    const x = document.querySelectorAll("span");
+export function getArchitects() {
+    let x = document.querySelectorAll("a");
+    let y = document.querySelectorAll("span");
     return [x, y]
 }
 
-export const getClassical = () => {
-    const y = document.querySelectorAll("classical");
-    const x = document.querySelectorAll("baroque modern");
+export function getClassical() {
+    let x = document.querySelectorAll('.classical');
+    let y = document.querySelectorAll('.modern, .baroque');
     return [x, y]
 }
 
-export const getActive = () => {
-    const y = document.querySelectorAll("true");
-    const x = document.querySelectorAll("false");
+export function getActive() {
+    let x = document.querySelectorAll('.classical, .true');
+    let y = document.querySelectorAll('.classical, .false');
+    console.log([x, y])
     return [x, y]
 }
 
-export const getBonannoPisano = () => {
-    const y = document.getElementById("BonannoPisano");
-    const x = document.querySelectorAll("classical true");
-    console.log([y,x])
-    return [y, x]
-    
-
+export function getBonannoPisano() {
+    let x = document.getElementById("BonannoPisano");
+    let y = document.querySelectorAll("true, .classical");
+    console.log([x, y])
+    return [x, y]
 }
