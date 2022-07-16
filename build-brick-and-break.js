f84b45f1715b54f2792753af2cf5fa2763273d29
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-16 14:48:39 +0100

    saturday

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,18 +1,50 @@
-// <div id="brick-1"></div>
+let interval = setInterval(build, 100);
+let a = 1
+let globalNum
 
-export function build(bricksAmount) {
-    let brick = document.createElement("brick");
-    console.log(brick)
-    brick.setAttribute('id', 'brick-1')
-    let finalResult = setInterval(brick, 100);
-    console.log(finalResult)
-    return finalResult
+export function build(num) {
+    if (num != undefined && num != globalNum) {
+        globalNum = num
+    }
+    let brick = document.createElement("div");
+    brick.id = "brick-" + a
+    a++
+    if (a % 2 !== 0) {
+        brick.setAttribute('foundation', 'true')
+    }
+    document.body.appendChild(brick)
+    if (a == globalNum + 1) {
+        clearInterval(interval)
+        return
+    }
+    // console.log(brick)
 }
 
-export function repair(ids) {
 
+// <div id="brick-1" foundation="true"></div>
+export function repair(ids) {
+    let x = document.querySelectorAll(ids)
+    // let y = document.querySelectorAll("foundation")
+    let z = document.hasAttribute('foundation')
+    if (z) {
+        z.setAttribute('repaired', 'in progress')
+    } else {
+        z.setAttribute('repaired', 'true')
+    }
+    document.body.appendChild(x)
+    console.log(x)
+    return x
 }
 
 export function destroy(ids) {
 
 }
+
+// function split(s) {
+//     for (let i = 0; i < s.length; i++) {
+//         let num = parseInt(s[i].split('-'));
+//         if (typeof (num) == 'number') {
+//             return num
+//         }
+//     }
+// }
