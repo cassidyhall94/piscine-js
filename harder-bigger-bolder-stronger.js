699ac940aada81ac85da38f4c9442c0b03c94404
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-18 14:34:39 +0100

    monday

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -7,27 +7,19 @@ export function generateLetters() {
         // if i is less than 41
         if (i < 40) {
             let weight = letter.style.fontWeight = 300
-            // letter.style.fontSize = 11 + i + 'px'
-            // console.log(letter, i, weight)
         }
         // if i is greater than 40, and less than 80
         if (i < 80 && i > 39) {
             let weight = letter.style.fontWeight = 400
-            // let font = letter.style.fontSize = 11 + i + 'px'
-            // console.log(letter, i, weight)
         }
         // if i is greater than 80, and less than 120
         if (i < 120 && i > 79) {
             let weight = letter.style.fontWeight = 600
-            // let font = letter.style.fontSize = 11 + i + 'px'
-            // console.log(letter, i, weight)
         }
         if (i < 121 && i > -1) {
             let font = letter.style.fontSize = 11 + i + 'px'
-            // console.log(letter, font)
         }
         document.body.appendChild(letter)
-        console.log(letter, i)
     }
 }
 
--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,11 +1,25 @@
 import { styles } from './pimp-my-style.data.js';
 
+let stylesCounter = 0;
+let removeCounter = 14;
+
 export function pimp() {
     let x = document.getElementsByClassName("button");
-    console.log('x:', x)
-    let y = x.classList.add(styles)
-    console.log('y:', y)
-    // for (let i = 0; i < styles.length; i++) {
-    //     x.add(styles[i])
-    // }
+    for (const y of x) {
+        if (stylesCounter < 15) {
+            y.classList.add(styles[stylesCounter])
+        } else {
+            y.classList.remove(styles[removeCounter])
+        }
+        if (stylesCounter === 14 && removeCounter === 14) {
+            y.classList.add('unpimp')
+        } else if (stylesCounter === 15 && removeCounter === 0) {
+            y.classList.remove('unpimp')
+        }
+    }
+    if (stylesCounter < 15) {
+        stylesCounter++
+    } else if (stylesCounter === 15 && removeCounter > -1) {
+        removeCounter--
+    }
 }
