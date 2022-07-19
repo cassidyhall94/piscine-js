b19a682ddb2e6da3f1ca2d2593432e138410f136
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-19 15:38:45 +0100

    tuesday

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,31 +1,60 @@
 let circle
+let box
 
 export function createCircle() {
     document.addEventListener('click', (event) => {
         let x = document.createElement('div');
         x.classList.add('circle')
-        x.style.backgroundColor = 'white'
-        x.style.left = event.clientX + 'px';
-        x.style.top = event.clientY + 'px';
+        x.style.background = 'white'
+        x.style.left = (event.clientX - 25) + 'px';
+        x.style.top = (event.clientY - 25) + 'px';
         x.classList.add('following')
         circle = x
-        document.body.appendChild(x)
+        document.body.appendChild(circle)
     }, false);
 }
 
 export function moveCircle() {
     addEventListener('mousemove', (event) => {
-        // let circle = document.getElementsByClassName('following').item(0)
-        // circle.style.transform = `translateX((${event.movementX}-25)+ 'px')`
-        // circle.style.transform = `translateY((${event.movementY}-25)+ 'px')`
-        // console.log(circle)
-        circle.style.left = (event.clientX - 25) + 'px';
-        circle.style.top = (event.clientY - 25) + 'px';
+        const brE = circle.getBoundingClientRect()
+        const brC = box.getBoundingClientRect()
+        if (circle.style.background === 'var(--purple)') {
+            if (event.clientX < brC.left + 1) {
+                circle.style.left = brC.left + 1 + 'px'
+            } else {
+                circle.style.left = (event.clientX - 25) + 'px'
+            }
+
+            if (event.clientY < brC.top + 1) {
+                circle.style.top = brC.top + 1 + 'px'
+            } else {
+                circle.style.top = (event.clientY - 25) + 'px'
+            }
+            if (event.clientY + 25 > brC.bottom - 1) {
+                circle.style.top = (brC.bottom - 1) - 50 + 'px'
+            }
+            if (event.clientX + 25 > brC.right - 1) {
+                circle.style.left = (brC.right - 1) - 50 + 'px'
+            }
+        } else {
+            circle.style.left = (event.clientX - 25) + 'px';
+            circle.style.top = (event.clientY - 25) + 'px';
+        }
+        if (
+            brE.left >= brC.left &&
+            brE.top >= brC.top &&
+            brE.bottom <= brC.bottom &&
+            brE.right <= brC.right
+        ) {
+            circle.style.background = 'var(--purple)'
+        }
     });
 }
 
+
 export function setBox() {
     let x = document.createElement('div');
-        x.classList.add('box')
-    document.body.appendChild(x)
+    x.classList.add('box')
+    box = x
+    document.body.appendChild(box)
 }
