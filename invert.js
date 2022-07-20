f409d61a1f6890e265c0c1e5f609aae8c87c34f0
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-20 13:02:28 +0100

    wednesday

--- invert.js
+++ invert.js
@@ -1,24 +1,9 @@
-function invert(obj,fn) {
-    Object.keys(obj).reduce((acc, key) => {
-        const val = fn ? fn(obj[key]) : obj[key];
-        acc[val] = acc[val] || [];
-        acc[val].push(key);
-        return acc;
-    }, {});
+function invert(obj) {
+    var returnObj = {}
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            returnObj[obj[key]] = key;
+        }
+    }
+    return returnObj;
 }
-
-// function get(key) {
-//     for (var k in sourceObject) {
-//         if (k == key)
-//         return sourceObject[k]
-//     }
-// }
-
-// function set(key, value) {
-//         // console.log(k)
-//         // console.log(key)
-//         sourceObject[key] = value
-//         // console.log(sourceObject[k])
-//         // console.log(value)
-//         return sourceObject[key]
-// }
