967e120dd242a7e8719bee1c8d7bf6ca5257fe2c
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-20 18:26:00 +0100

    wednesday

--- pick-omit.js
+++ pick-omit.js
@@ -1,14 +1,35 @@
 function pick(obj, str) {
-    for (let i = 0; i < str.length; i++) {
+    let result = {}
+    let arr = []
+    if (typeof str === 'string') {
+        arr.push(str)
+    } else {
+        arr = str
+    }
+    for (let i = 0; i < arr.length; i++) {
         for (var k in obj) {
-            if (k == str[i]) {
-                return obj
+            if (k === arr[i]) {
+                result[k] = obj[k]
             }
         }
     }
+    return result
 }
 
 function omit(obj, str) {
+    let result = {}
+    let arr = []
+    if (typeof str === 'string') {
+        arr.push(str)
+    } else {
+        arr = str
+    }
 
+    for (var k in obj) {
+        if (!arr.includes(k) && Object.hasOwn(obj, k)) {
+            result[k] = obj[k]
+        }
+    }
+    return result
 }
 
