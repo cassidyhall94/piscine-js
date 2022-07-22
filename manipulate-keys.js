a40d7f2e34732c25e75d93965c28a0751daffc9d
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-07-22 12:01:58 +0100

    friday

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -1,23 +1,27 @@
 function filterKeys(obj, func) {
-    // console.log('FILTER OBJ:', obj)
+    console.log('FILTER OBJ:', obj)
     let result = {}
     for (let k in obj) {
         if (func(k)) {
             result[k] = obj[k]
         }
     }
-    // console.log('FILTER RESULT', result)
+    console.log('FILTER RESULT', result)
     return result
 }
 
 function mapKeys(obj, func) {
-    // console.log('OBJ:', obj)
+    console.log('OBJ:', obj)
     for (let k in obj) {
         let keys = func(k)
-        // console.log('KEYS:', keys)
+        console.log('KEYS:', keys)
         delete Object.assign(obj, { [keys]: obj[k] })[k];
+        if (k === 'onion') {
+            console.log('onion')
+            delete obj[k]
+        } 
     }
-    // console.log('RESULT', obj)
+    console.log('**RESULT', obj)
     return obj
 }
 
@@ -27,19 +31,16 @@ function reduceKeys(obj, func) {
     return result
 }
 
-// const cart = {
-//     vinegar: 80,
-//     sugar: 100,
-//     oil: 50,
-//     onion: 200,
-//     garlic: 22,
-//     paprika: 4,
-// }
+const cart = {
+    vinegar: 80,
+    sugar: 100,
+    oil: 50,
+    onion: 200,
+    garlic: 22,
+    paprika: 4,
+}
 
 
-// console.log(mapKeys(
-//     filterKeys(cart, (k) => k === 'onion'),
-//     (k) => (k = 'orange'),
-// ))
+console.log(mapKeys(filterKeys(cart, (k) => k === 'onion'), (k) => (k = 'orange')))
 
 // console.log(reduceKeys(cart, (acc, cr) => acc.concat(', ', cr)))
